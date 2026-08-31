f84157f902bb41e76bf59c9bf8e15395791d21a8
unknown <david.buono@protonmail.com>
2026-08-31 18:44:59 +0200

    Transpose repo

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import Tab from "./Tab.jsx"
+import './App.css';
+
+function App() {
+
+	//URL PARAMETERS
+	const CORSproxy = "https://proxy.corsfix.com/";
+	
+	//STATE
+	const [currentUrlTab, setCurrentUrlTab] = useState("https://proxy.corsfix.com/https://tabs.ultimate-guitar.com/tab/billie-eilish/happier-than-ever-chords-3592094");
+
+	return (
+		<div className="App">
+			<h1>Ultimate guitar PDF downloader</h1>
+			<div id="examples">
+				<input size="100" name="myInput" onChange={(newUrl) => {
+					if(JSON.stringify(newUrl["nativeEvent"]["data"]).includes("tabs.ultimate-guitar.com")) {
+						setCurrentUrlTab(CORSproxy + newUrl["nativeEvent"]["data"])
+					}}}/>
+			</div>
+			<Tab url={currentUrlTab} />
+		</div> 
+	);
+}
+
+export default App
---
+++ src/Tab.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react'
+import axios from "axios";
+import * as cheerio from "cheerio";
+
+function Tab(props){
+
+    const [data, setData] = useState([]); //RAW EXTRACTED DATA
+	const [loaded, setLoaded] = useState(false); //LOADED PAGE
+	const [jsData, setJsData] = useState(); //JS DATA
+	//const [justOneReload, setJustOneReload] = useState(true); 
+	
+    
+    console.log("Tab executed!");
+
+    useEffect( () => {
+        console.log("Axios executed!");
+		axios({ method: "get", url: props.url })
+		.then(response => {setData(response.data); setLoaded(true)})
+	} , [props.url] );
+	
+	if(loaded){
+        try{
+            console.log("Loaded!");
+            const $ = cheerio.load(data);
+            const fullJson = JSON.parse( $("[data-content]").attr("data-content") );
+            const jsonTab = fullJson["store"]["page"]["data"]["tab_view"]["wiki_tab"]["content"]
+            let stringTab = JSON.stringify(jsonTab);
+            stringTab = stringTab.replaceAll("[tab]", "").replaceAll("[/tab]", "").replaceAll("[ch]", "<b>").replaceAll("[/ch]","</b>");
+
+            let jsonTabModified = JSON.parse(stringTab);
+            
+            setJsData(jsonTabModified);
+            setLoaded(false);
+        } catch (err){
+            console.error(err);
+        }
+	}
+
+    return(
+        <div id="tab-content">
+            {jsData !== undefined ? <pre><div dangerouslySetInnerHTML={{ __html: jsData }} /></pre> : <pre>Loading...</pre>}
+        </div>	
+    );
+}
+
+export default Tab;
